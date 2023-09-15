f715b6f3b3e067a68e24af6f7423d4d632741e3c
Souzaa <zinfegr@gmail.com>
2023-09-15 18:59:45 -0300

    Correção bug imagem

--- src/layouts/amount.jsx
+++ src/layouts/amount.jsx
@@ -2,34 +2,31 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import High from "./high";
 
-
 function Amount() {
-    const [saldo, setSaldo] = useState(0);
+  const [saldo, setSaldo] = useState(0);
 
-    return (
-        <div id="amount">
-            <High />ㅤ
+  return (
+    <div id="amount">
+      <High />ㅤ
+      <div id="info">
+        <h2>Saldo PicPay</h2>
+        <h1>R$ {saldo}</h1>
+      </div>
+      <div id="options">
+        <Link to="/pix" className="option" id="pix">
+          <img src="./src/imgs/pix.svg" /> <span>Pix</span>
+        </Link>
 
-            <div id="info">
-                <h2>Saldo PicPay</h2>
-                <h1>R$ {saldo}</h1>
-            </div>
+        <Link to="/depositar" className="option" id="deposito">
+          <img src="./src/imgs/deposit.svg" /> <span>Depositar</span>
+        </Link>
 
-            <div id="options">
-                <Link to='/pix' className="option" id="pix" >
-                    <img src="/src/imgs/pix.svg" /> <span>Pix</span>
-                </Link>
-                
-                <Link to='/depositar' className="option" id="deposito" >
-                    <img src="/src/imgs/deposit.svg" /> <span>Depositar</span>
-                </Link>
-
-                <Link to='/transacoes' className="option" id="transacoes">
-                    <img src="/src/imgs/transacoes.svg" /> <span>Transações</span>
-                </Link>
-            </div>
-        </div>
-    )
+        <Link to="/transacoes" className="option" id="transacoes">
+          <img src="./src/imgs/transacoes.svg" /> <span>Transações</span>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Amount;
