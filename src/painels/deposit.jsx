afdf65f632f0d2cfe9e727628e1741d434587c3a
Souzaa <zinfegr@gmail.com>
2023-09-19 00:15:21 -0300

    update 'Depositar'

--- src/painels/deposit.jsx
+++ src/painels/deposit.jsx
@@ -1,16 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Deposit() {
-    const [saldo, setSaldo] = useState(0);
+    const [valor, setValor] = useState();
     
     return (
         <div id="depositarPainel" className="painel">
             <Link to='/' style={{width: '50px'}}><img src="../src/imgs/back.svg" style={{filter: 'invert()'}} /></Link>
-            <h2>Depositar</h2>
-            <h2>R${saldo}</h2>
-            <input type="number" placeholder="Valor do deposito" />
-            <button onClick={()=> setSaldo(0)}>Deposit</button>
+            <h2>Depositar R${valor}</h2>
+            <input type="number" value={valor} onChange={(e)=>setValor(e.target.value)} placeholder="Valor do deposito" />
+            <button onClick={()=> setValor('')}>Depositar</button>
         </div>
     )
 }
